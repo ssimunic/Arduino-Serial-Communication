7408bb5bf45b4f19c085e0ede312b25d4b04a850
Silvio Simunic <silvio.simunic@gmail.com>
2014-12-26 02:21:15 +0100

    Initial commit.

--- arduinoCommunication/Properties/AssemblyInfo.cs
+++ arduinoCommunication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("arduinoC")]
+[assembly: AssemblyTitle("Arduino Communication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("arduinoC")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Arduino Communication")]
+[assembly: AssemblyCopyright("Copyright © Silvio Simunic")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
